1a3883adfc4661b3ee1b9b256d5cb8b0efc15303
Mishra Prince <indiandexter007@gmail.com>
2020-04-06 03:36:27 +0530

    Initial commit

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/prince-mishra/selenium_fb_group_automator',
-    version='0.1.0',
+    version='0.1.1',
     zip_safe=False,
 )
